2345179182c3ee57d7ea93d58d9ec5e754c653aa
Mihael Pačnik <mihapac@gmail.com>
2016-10-26 13:13:04 +0200

    Celo nekaj podatkov sem dobil

---
+++ Kategorije_iskati.py
@@ -0,0 +1,35 @@
+import re
+import csv
+import orodja
+
+#to išče kategorije, in jih da v csv datoteko, imel bi še idejo, da bi dodal število izdelkov na prodajo, a ker se to
+#število stalno spreminja bom moral to narediti takrat ko iscem produkte, in še takrat ne natančno
+
+#kategorije = open("kategorije.html", 'r')
+
+#vzeto iz predavanj
+
+regex_kategorije = re.compile(
+    r'<li><a href="http://www.ebay.com/sch/(?P<html_ime>\D+?.*?)/(?P<id>\d+)/i.html" class="ch">(?P<ime>.*?)</a></li>',
+    flags=re.DOTALL
+)
+
+
+def pocisti_kategorije(kategorija):
+    podatki = kategorija.groupdict()
+    podatki['id'] = int(podatki['id'])
+    podatki['html_ime'] = (podatki['html_ime'])
+    podatki['ime'] = (podatki['ime'])
+    return podatki
+
+
+def izloci_podatke_kategorij(imenik):
+    kategorije = []
+    for html_datoteka in orodja.datoteke(imenik):
+        for kategorija in re.finditer(regex_kategorije, orodja.vsebina_datoteke(html_datoteka)):
+            kategorije.append(pocisti_kategorije(kategorija))
+    return kategorije
+
+
+kategorije = izloci_podatke_kategorij('Data/Kategorije/')
+orodja.zapisi_tabelo(kategorije, ['id', 'html_ime', 'ime'], 'Data/Kategorije/kategorije.csv')
